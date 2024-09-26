68466ebeee46ce939bdd73baca896edec73df389
WIN10\vboxuser <tspartner21@gmail.com>
2024-09-26 17:16:18 +0900

    toggle traffic button

--- js/kakao.js
+++ js/kakao.js
@@ -13,19 +13,32 @@ const mapOption = {
   //출력할 지도의 위도,경도
   level: 2 // 지도의 확대 레벨
 };
-
+//스크립트가 처음 로드된 시점에 mapOption안에 포함되어 있는 위치값을 기준으로 지도 인스턴스가 생성되고 끝
+//이슈사항 :지도 인스턴스가 처음 생성된 시점에서 위치값이 고정되어 있기 때문에 리사이즈시 위치 중앙이 틀어짐
+//해결방법 : 브라우저 리사이즈 할때마다 지도 인스턴스를 생성
 //map instance 
-const map = new kakao.maps.Map(mapContainer, mapOption);
-
+//브라우저 리사이즈 될때마다 map 변수에 변경된 값을 재반영해야 되므로 let방식으로 변수선언
+let map = new kakao.maps.Map(mapContainer, mapOption);
 //코드 정리
 //마커에 대한 인스턴스 생성(인수로 위에서 생성된 지도위치 인스턴스 필요)
-var marker = new kakao.maps.Marker({
+let marker = new kakao.maps.Marker({
   position: mapOption.center //mapOption 개체 인스턴스 사용
 });
-
 //마커 인스턴스에 setMap 함수를 호출해서 인수로 지도 인스턴스 집어넣음
 // 마커가 지도 위에 표시되도록 설정합니다
 marker.setMap(map);
+
+//브라우저 리사이즈이벤트가 발생할ㅇ때마다
+window.addEventListener("resize", () => {
+  map = new kakao.maps.Map(mapContainer, mapOption);
+  //마커 인스턴스에 setMap 함수를 호출해서 인수로 지도 인스턴스 집어넣음
+  // 마커가 지도 위에 표시되도록 설정합니다
+  marker.setMap(map);
+});
+
+
+
+
 
 // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
 const mapTypeControl = new kakao.maps.MapTypeControl();
